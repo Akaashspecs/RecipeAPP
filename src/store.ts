6ff7c399ce1b2689311c3d7d67e6654052b971d2
Aakashspecs <aakashspecs@gmail.com>
2024-04-08 11:14:02 -0700

    remove unused codes

--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,8 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-// import ShowReducer from "./reducers/Shows";
+import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
-import { takeLatest, takeEvery, all, fork, debounce } from "redux-saga/effects";
 import createSagaMiddleware from "redux-saga";
 import rootReducer from "./store/reducers";
 import rootSaga from "./store/saga";
-// import { SHOWS_QUERY_CHANGE, SINGLE_SHOW_ID_CHANGE } from "./actions/Shows";
-// import { fetchShows, fetchSingleShow } from "./sagas/Shows";
-// import { fetchCast } from "./sagas/Cast";
-// import CastReducer from "./reducers/Casts";
 
 const sagaMiddleware = createSagaMiddleware();
 
